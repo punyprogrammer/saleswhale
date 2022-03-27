b19618e3cd1b7126e3d527c67496885081106e0e
punyprogrammer <johnmayer190999@gmail.com>
2022-03-27 22:27:35 +0530

    fixed-skelton

--- src/components/SkeletonSingleTeam.jsx
+++ src/components/SkeletonSingleTeam.jsx
@@ -1,9 +1,5 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import ActiveStar from "../assets/star-active.svg";
-import DefaultStar from "../assets/star-default.png";
-import LeadsIcon from "../assets/icon-leads-small.png";
-import CampaignIcon from "../assets/icon-conversations-small.png";
 const Container = styled.div`
   background: #ffffff;
   border: 1px solid #e4e7ec;
@@ -51,11 +47,7 @@ const TeamCreatedAt = styled.div`
 
   color: #565656;
 `;
-const TeamTopStar = styled.img`
-  transition: all 0.3s ease;
-  position: absolute;
-  right: 0;
-`;
+
 const Center = styled.div`
   border-bottom: 1px solid #e4e7ec;
   padding: 18px 0px;
@@ -67,64 +59,10 @@ const TeamCenterDesc = styled.div`
   background-color: #a19f9f;
   border-radius: 4px;
 `;
-const TeamCenterDescText = styled.p`
-  font-size: 14px;
-  line-height: 17px;
-  /* or 121% */
-
-  color: #565656;
-  text-overflow: ellipsis;
-  display: -webkit-box;
-  overflow: hidden;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-`;
 const Bottom = styled.div`
   display: flex;
   align-items: center;
   padding: 20px 0px;
-`;
-const CampaignImage = styled.div`
-  margin-right: 6px;
-  background-color: #a19f9f;
-  height: 20px;
-  width: 20px;
-`;
-const CampaignText = styled.span`
-  font-size: 13px;
-  line-height: 16px;
-  background-color: #a19f9f;
-
-  /* identical to box height, or 123% */
-
-  color: #000000;
-  font-weight: 500;
-
-  mix-blend-mode: normal;
-  opacity: 0.45;
-`;
-const LeadsImage = styled.img`
-  margin-right: 6px;
-  margin-left: 20px;
-  background-color: #a19f9f;
-`;
-const LeadsText = styled.span`
-  font-size: 13px;
-  line-height: 16px;
-  background-color: #a19f9f;
-
-  /* identical to box height, or 123% */
-
-  color: #000000;
-
-  mix-blend-mode: normal;
-  opacity: 0.45;
-  font-weight: 500;
-`;
-const IconContainer = styled.div`
-  height: 30px;
-  width: 30px;
-  background-color: #a19f9f;
 `;
 const BottomText = styled.div`
   background-color: #a19f9f;
